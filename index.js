95d2effc0d83b1836bdfbc1fa1b81fbb65cd632a
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-01-07 12:20:36 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+
+let allSides = [5, 5, 5]
+class Polygon {
+        constructor(allSides){
+        this.sideA = allSides[0];
+        this.sideB = allSides[1];
+        this.sideC = allSides[2];
+        this.allSides = allSides
+    }
+    get countSides (){ return this.allSides.length }
+    // get perimeter() { return this.sideA + this.sideB + this.sideC}
+    get perimeter() {return this.allSides.reduce((a, b) => a + b)}
+    // get perimeter() { return this.sideA + this.sideB + this.sideC || allSides.reduce((a, b) => a + b)}
+}
+
+class Triangle extends Polygon{
+    get isValid() { 
+        return ((this.sideA + this.sideB) > this.sideC && (this.sideB + this.sideC) > this.sideA && (this.sideA + this.sideC) > this.sideB) ? true : false
+    }
+}
+
+class Square extends Polygon{
+    constructor(allSides){
+        super(allSides)
+        this.sideD = allSides[3]
+    }
+    get area() { return this.sideA * this.sideA};
+    get isValid() { return (this.perimeter/4 === this.sideA) ? true: false};
+}
+
+
+
+
